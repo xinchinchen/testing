43085ee975b99b0fe675154a5d1bd6f57b7f8911
xinchinchen <xinchinchen@gmail.com>
2018-12-08 00:55:54 +0800

    Updates

---
+++ precache-manifest.2bc74d9cefbb79017e5bbf447bb73647.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1915a2c2a9d1bdff14f9",
+    "url": "/testing/static/css/main.3d4f155b.chunk.css"
+  },
+  {
+    "revision": "1915a2c2a9d1bdff14f9",
+    "url": "/testing/static/js/main.1915a2c2.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/testing/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "47854e5355f3c459be53",
+    "url": "/testing/static/js/runtime~main.47854e53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testing/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "eec8c70d1a247497e78ac60dc4453223",
+    "url": "/testing/index.html"
+  }
+];
