10b8535842ebe7fffb40d8bfe29a278e52b8fd33
xinchinchen <xinchinchen@gmail.com>
2018-12-08 02:14:54 +0800

    testing

---
+++ src/projects/index.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line
+import project2017fall from '!json-loader!yaml-loader!./2017fall.yaml'
+// eslint-disable-next-line
+import project2018fall from '!json-loader!yaml-loader!./2017fall.yaml'
+const projects = [
+    project2018fall,
+    project2017fall,
+]
+
+export default projects
